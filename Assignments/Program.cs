798212ac0dc144cb52881784b53fd201d85f31fa
BrundaMahesh <brunda.mahesh@ust.com>
2023-10-18 17:55:18 +0530

    modified

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Assignments;
+
+Student student = new Student("RajKumar", 60, 70, 50);
+Console.WriteLine($"Total=" +student.Total());
+Console.WriteLine($"Average=" +student.CalculateAverage());
+
---
+++ Assignments/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class Student
+    {
+        string fullName,grade;
+        int mark1, mark2,mark3,total;
+        double average;
+
+        public Student(string fullName, int mark1, int mark2, int mark3)
+        {
+            this.fullName = fullName;
+            this.mark1 = mark1;
+            this.mark2 = mark2;
+            this.mark3 = mark3;
+        }
+
+        public double CalculateAverage()
+        {
+            int total=mark1 + mark2 + mark3;
+            double average = total / 3;
+            return average;
+        }
+        public double Total()
+        {
+            int total = mark1 + mark2 + mark3;
+            return total;
+        }
+    }
+}

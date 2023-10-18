49a437e139bf621edef03878f3532052cf6988ae
BrundaMahesh <brunda.mahesh@ust.com>
2023-10-18 17:13:05 +0530

    Add project files.

---
+++ Basic Programs/Program.cs
@@ -0,0 +1,62 @@
+﻿//*********Addition of two numbers**********//
+//Console.WriteLine("Enter the two numbers:");
+//int firstNumber,secondNumber,Sum;
+//firstNumber = Convert.ToInt32(Console.ReadLine());  
+//secondNumber = Convert.ToInt32(Console.ReadLine());
+//Sum=firstNumber + secondNumber;
+//Console.WriteLine("Sum of two numbers=" + Sum);
+
+
+
+//*************String Manipulation************//
+//string? s1 = "";
+//string? s2 = "";
+//string? s3 = "";
+//s1=Console.ReadLine();
+//s2=Console.ReadLine();
+//s3 = s1 + "" + s2;
+//Console.WriteLine(s3);
+//Console.WriteLine(s1.Equals(s2));
+//Console.WriteLine(s1.ToUpper());
+//Console.WriteLine(s1.ToLower());
+//Console.WriteLine(s2.Substring(3));
+
+//string[] s=s3.Split(' ');
+//for(int i = 0; i < s.Length; i++)
+//    Console.WriteLine(s[i]);
+
+
+//**************Switch expression******************//
+//int x = 1;
+//switch (x)
+//{
+//    case 0:
+//        Console.WriteLine("Zero");
+//        break;
+
+//    case 1:
+//        Console.WriteLine("One");
+//        break;
+
+//    default:
+//        Console.WriteLine("Invalid");
+//        break;
+//}
+
+
+
+//*************driver*********************//
+using Basic_Programs;
+
+//Calculation calculation = new Calculation();
+//int firstNumber = Convert.ToInt32(Console.ReadLine());
+//int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//double sum = calculation.Add(firstNumber, secondNumber);
+//Console.WriteLine(sum);
+
+
+
+//Electricity electricity = new Electricity(); //default constructor
+Electricity electricity= new Electricity(10001,9000,9300,"Raj"); //parameterised constructor
+Console.WriteLine($"Bill Amount=" +electricity.CalculateBill());

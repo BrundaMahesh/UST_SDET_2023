27b60efc52072750afa410952777d8cb3566204d
BrundaMahesh <brunda.mahesh@ust.com>
2023-10-19 15:58:01 +0530

    added Product class

---
+++ Assignments/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class Product
+    {
+        string productName;
+        double price;
+        int quantity;
+
+        public string ProductName { get => productName; set => productName = value; }
+        public double Price { get => price; set => price = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+
+        public Product(string productName, double price, int quantity)
+        {
+            this.ProductName = productName;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+
+        public void SetPrice(double newPrice) 
+        { 
+            Price=newPrice;
+        }
+        public double ProductValue()
+        {
+            return  Price*Quantity;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Product Name:" + ProductName);
+            Console.WriteLine($"Price:" + Price);
+            Console.WriteLine($"Quantity:" + Quantity);
+        }
+    }
+}
--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,6 +1,25 @@
 ﻿using Assignments;
 
-Student student = new Student("RajKumar", 50, 80, 40);
-Console.WriteLine($"Total=" +student.Total());
-Console.WriteLine($"Average=" +student.CalculateAverage());
-Console.WriteLine($"Grade=" + student.Grade());
+//Student student = new Student("RajKumar", 50, 80, 40);
+//Console.WriteLine($"Total=" +student.Total());
+//Console.WriteLine($"Average=" +student.CalculateAverage());
+//Console.WriteLine($"Grade=" + student.Grade());
+
+
+Product[] products = new Product[3];
+products[0] = new Product("Bread", 50, 2);
+products[1] = new Product("Milk",30,1);
+products[2] = new Product("Jam",80,1);
+Console.WriteLine("Product details");
+Console.WriteLine("***************\n");
+foreach (var item in products)
+{
+    item.Display();
+    Console.WriteLine();
+}
+
+Console.WriteLine("Total value:");
+foreach(var item in products)
+{
+    Console.WriteLine("Product:" +item.ProductName +" "+item.ProductValue());
+}

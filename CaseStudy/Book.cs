86d0468d7c233f621eac52bad6210420bc2fbd8c
BrundaMahesh <brunda.mahesh@ust.com>
2023-10-21 12:11:50 +0530

    added new project called CaseStudy

---
+++ CaseStudy/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy
+{
+    internal class Book
+    {
+        public string? Title {  get; set; }
+        public string? Author { get; set; }
+        public double? ISBN { get; set; }
+        public double Price { get; set; }
+        public string? Availability {  get; set; }
+    }
+}
---
+++ CaseStudy/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy
+{
+    internal class Order
+    {
+        public string[] ListOfBooks { get; set; }
+        public int OrderDate {  get; set; }
+        public double TotalPrice { get; set; }
+    }
+}

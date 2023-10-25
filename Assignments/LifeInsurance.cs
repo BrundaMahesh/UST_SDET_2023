f83d3948c4869aefea4bcb741918adcd586c3e88
BrundaMahesh <brunda.mahesh@ust.com>
2023-10-25 15:26:49 +0530

    added 2 new class

---
+++ Assignments/LifeInsurance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class LifeInsurance : InsurancePolicy1
+    {
+        public int Age {  get; set; }
+        public LifeInsurance(string? policyName, int policyId, double premiumAmount, int age)
+            : base(policyName, policyId, premiumAmount)
+        {
+            Age = age;
+        }
+
+        public override double CalculatePremium()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

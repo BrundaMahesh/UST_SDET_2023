c2f8c7a6e1735425d90e83d601ca738a6292f408
BrundaMahesh <brunda.mahesh@ust.com>
2023-11-02 15:13:11 +0530

    created new class Hotel

---
+++ Assignments/Hotel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class Hotel:TourismDestination1
+    {
+        public Hotel(string? name, string? location, int rating, double pricePerNight,string? hotelName,int availableRooms) 
+            : base(name, location, rating, pricePerNight)
+        {
+            HotelName = hotelName;
+            AvailableRooms = availableRooms;
+        }
+
+        public string? HotelName {  get; set; }
+        public int AvailableRooms {  get; set; }
+    }
+}

06d50f343e4fb22454e41dc528a8fbae583296d5
BrundaMahesh <brunda.mahesh@ust.com>
2023-11-02 10:06:40 +0530

    added new project Thread

---
+++ ThreadsExample/Program.cs
@@ -0,0 +1,21 @@
+﻿Thread thread1 = new Thread(T1);
+Thread thread2 = new Thread(T1);
+
+// Start the threads
+thread1.Start();
+thread2.Start();
+
+// Wait for the threads to finish
+thread1.Join();
+thread2.Join();
+
+Console.WriteLine("Both threads have completed their work.");
+
+static void T1()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        Console.WriteLine("Thread " + Thread.CurrentThread.ManagedThreadId + " is working: " + i);
+        Thread.Sleep(100); // Simulate some work
+    }
+}

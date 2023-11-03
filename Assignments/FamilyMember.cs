beac44410d30d40ba651c53dcfc1c1317d098f44
BrundaMahesh <brunda.mahesh@ust.com>
2023-11-03 16:45:40 +0530

    added new class FamilyMember

---
+++ Assignments/FamilyMember.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class FamilyMember
+    {
+        public string? Name { get; set; }    
+        public int Age { get; set; }
+        public static List<FamilyMember> Children { get; set;}
+
+
+
+    }
+}

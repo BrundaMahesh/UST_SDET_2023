5f54bff867be06a84b1cba248d63af4fe5f870c3
BrundaMahesh <brunda.mahesh@ust.com>
2023-11-04 10:02:18 +0530

    created new class Student and exception

---
+++ CaseStudy/CustomException/StudentException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy.CustomException
+{
+    internal class StudentException
+    {
+        public static Dictionary<string, string> ErrorMessages { get; set; } = new Dictionary<string, string>()
+        {
+
+            {"1","Product Id should be greater than zero"},
+            {"2","Product name cannot be null or empty"}
+
+        };
+    }
+}
+    internal class EnrollmentException : Exception
+    {
+        public EnrollmentException(string message) : base(message) { }
+    }
